5a3e91405c800a43010023affb3948acd900d133
blossun <dnsngu@gmail.com>
2020-09-23 14:12:02 +0900

    [example] 2.2.3 테스트 코드 중복 제거 - 추천하는 방식
    `@BeforeEach` 애노테이션을 활용해 다음과 같이 구혈할 것을 추천
    @Before 애노테이션을 사용하지 않고 필드로 구현하는 방법은 CalculatorTest의 인스턴스가 생성될 때 Calculator 인스턴스가 생성되어 모든 테스트 메소드가 **재사용하는 방식**으로 동작한다.
    
    이와 같이 재사용될 경우 앞서 테스트 메소드를 실행하면서 Calculator의 상태 값이 변경되어 다음 테스트를 실행할 때 영향을 미칠 수 있기 때문이다.

--- step2/src/test/java/dev/solar/CalculatorTest.java
+++ step2/src/test/java/dev/solar/CalculatorTest.java
@@ -1,29 +1,46 @@
 package dev.solar;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculatorTest {
-    private Calculator cal = new Calculator();
+    private Calculator cal;
+
+    @BeforeEach
+    void setUp() {
+        cal = new Calculator();
+        System.out.println("before");
+    }
 
     @Test
     void add() {
-        assertEquals(9, cal.add(6,3));
+        assertEquals(9, cal.add(6, 3));
+        System.out.println("add");
     }
 
     @Test
     void subtract() {
-        assertEquals(3, cal.subtract(6,3));
+        assertEquals(3, cal.subtract(6, 3));
+        System.out.println("subtract");
     }
 
     @Test
     void multiply() {
-        assertEquals(18, cal.multiply(6,3));
+        assertEquals(18, cal.multiply(6, 3));
+        System.out.println("multiply");
     }
 
     @Test
     void divide() {
-        assertEquals(2, cal.divide(6,3));
+        assertEquals(2, cal.divide(6, 3));
+        System.out.println("divide");
+    }
+
+    @AfterEach
+    void teardown() {
+        System.out.println("teardown");
     }
 }
