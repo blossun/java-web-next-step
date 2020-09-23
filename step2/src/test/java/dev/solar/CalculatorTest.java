92bd2f0866a41a6f5f026dea0e30c31dfac8a0c0
blossun <dnsngu@gmail.com>
2020-09-23 14:03:27 +0900

    [example] 2.2.3 테스트 코드 중복 제거 - 추천하지않는 방식
    JUnit은 테스트를 실행하기 위한 초기화 작업을 위와 같이 구현하는 것을 추천하지 않는다.

--- step2/src/test/java/dev/solar/CalculatorTest.java
+++ step2/src/test/java/dev/solar/CalculatorTest.java
@@ -5,28 +5,25 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    private Calculator cal = new Calculator();
 
     @Test
     void add() {
-        Calculator cal = new Calculator();
         assertEquals(9, cal.add(6,3));
     }
 
     @Test
     void subtract() {
-        Calculator cal = new Calculator();
         assertEquals(3, cal.subtract(6,3));
     }
 
     @Test
     void multiply() {
-        Calculator cal = new Calculator();
         assertEquals(18, cal.multiply(6,3));
     }
 
     @Test
     void divide() {
-        Calculator cal = new Calculator();
         assertEquals(2, cal.divide(6,3));
     }
 }
