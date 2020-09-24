7705ed8cd67f589bfc6fbac47ecf077ed818de07
blossun <dnsngu@gmail.com>
2020-09-24 16:53:57 +0900

    [example] 2.4.4.2 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환

--- step2/src/test/java/dev/solar/nextstep/StringCalculatorTest.java
+++ step2/src/test/java/dev/solar/nextstep/StringCalculatorTest.java
@@ -14,9 +14,13 @@ class StringCalculatorTest {
     }
 
     @Test
-    public void add_null_또는_빈문자() {
+    void add_null_또는_빈문자() {
         assertThat(cal.add(null)).isZero();
         assertThat(cal.add("")).isZero();
     }
 
+    @Test
+    void add_숫자하나() {
+        assertThat(cal.add("1")).isEqualTo(1);
+    }
 }
