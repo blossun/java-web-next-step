3f49561d2b1f8fc4bfc1d3674b3daacbadbbe00b
blossun <dnsngu@gmail.com>
2020-10-08 18:05:13 +0900

    feat: Part 7 - CSS 지원하기
    .css 확장자로 끝나는 요청에 대해 response header의 content-type 값을 text/css로 전달

--- web-application-server-master/src/main/java/webserver/RequestHandler.java
+++ web-application-server-master/src/main/java/webserver/RequestHandler.java
@@ -108,6 +108,14 @@ public class RequestHandler extends Thread {
                 byte[] body = sb.toString().getBytes();
                 response200Header(dos, body.length);
                 responseBody(dos, body);
+                return ;
+            }
+
+            if (url.endsWith(".css")) {
+                byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+                response200CssHeader(dos, body.length);
+                responseBody(dos, body);
+                return ;
             }
 
             responseResource(dos, url);
@@ -163,6 +171,17 @@ public class RequestHandler extends Thread {
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
+        }
+    }
+
+    private void response200CssHeader(DataOutputStream dos, int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/css\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
